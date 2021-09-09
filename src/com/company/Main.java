a0955e78324336d61ca30429e7dbf8cc69383e4d
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-09 18:16:54 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 dylan_nguyen
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+    }
+}
